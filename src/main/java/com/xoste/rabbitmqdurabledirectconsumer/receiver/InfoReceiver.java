26fad2d13f8fb0674851e80c3adbc74a7bb5ecd3
xoste <1594884214@qq.com>
2021-01-30 00:30:37 +0800

    Initial commit

---
+++ src/main/java/com/xoste/rabbitmqdurabledirectconsumer/receiver/InfoReceiver.java
@@ -0,0 +1,23 @@
+package com.xoste.rabbitmqdurabledirectconsumer.receiver;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * info消息接收者
+ * @author Leon
+ */
+@Component
+@RabbitListener(bindings = @QueueBinding(
+        exchange = @Exchange(value = "${rabbitmq.config.exchange}", type = ExchangeTypes.DIRECT),
+        key = "${rabbitmq.config.queue.info.routing.key}",
+        value = @Queue(value = "${rabbitmq.config.queue.info}", autoDelete = "false")
+))
+public class InfoReceiver {
+
+    @RabbitHandler
+    public void infoReceive(String msg) {
+        System.out.println("info......receive: " +msg);
+    }
+}
